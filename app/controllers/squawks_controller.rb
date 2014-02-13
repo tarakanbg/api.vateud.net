3cef7a474c9d7bb6f936ced058facf810da7db00
Svilen Vassilev <svilen@rubystudio.net>
2014-02-13 13:25:25 +0200

    hide free squawks

--- app/controllers/squawks_controller.rb
+++ app/controllers/squawks_controller.rb
@@ -1,19 +1,19 @@
 class SquawksController < ApplicationController
-  
+
   caches_action :index, expires_in: 4.hours
   caches_action :show, expires_in: 4.hours
-  
+
   def index
-    @pagetitle = "VATEUD approved squawk ranges"     
-    @squawks = Squawk.all
+    @pagetitle = "VATEUD approved squawk ranges"
+    @squawks = Squawk.active
     respond_to do |format|
-      format.html 
+      format.html
       format.json { render json: scope_json(@squawks) }
       format.xml { render xml: scope_xml(@squawks) }
       format.csv { send_data Squawk.to_csv }
     end
   end
-  
+
 private
 
   def scope_json(freqs)
@@ -23,5 +23,5 @@ private
   def scope_xml(freqs)
     freqs.to_xml(:only =>  [:facility, :position, :start, :end], skip_types: true)
   end
-  
+
 end
--- app/models/squawk.rb
+++ app/models/squawk.rb
@@ -4,19 +4,21 @@ class Squawk < ActiveRecord::Base
   self.abstract_class = true
 
   establish_connection("freq")
-  
+
   set_table_name "squawks"
 
   attr_accessible :facility, :position, :start, :end
 
   default_scope order('facility ASC')
 
+  scope :active, where('facility != "Free"')
+
 
   def self.to_csv(options = {})
     columns = ["facility", "position", "start", "end"]
     CSV.generate(options) do |csv|
       csv << columns
-      all.each do |sq|
+      active.each do |sq|
         csv << sq.attributes.values_at(*columns)
       end
     end
